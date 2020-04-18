82ec626efdebaf737d7b956ab2af9c8f5ebb22da
davidbudzynski <david.budzynsky@gmail.com>
2020-04-18 13:55:21 +0100

    added search bar and background particles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import Particles from "react-particles-js";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition />} */}
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5,
+      },
+    },
+  },
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition />} */}
+      </div>
+    );
+  }
 }
 
 export default App;
